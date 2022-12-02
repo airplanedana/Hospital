5bf1e1f3fda984943cec3f4bbdc571441b574689
airplanedana <dana.gomez@airplane.solutions>
2022-12-02 12:01:01 +0100

    Agregar archivos de proyecto.

---
+++ Hospital/Inicio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hospital
+{
+    internal class Inicio
+    {
+        public static void Main(string[] args)
+        {
+            // Instanciamos el manager para hacer todas las gestiones
+            Manager manager = new Manager();
+
+            manager.Iniciar();
+        }
+    }
+}
---
+++ Hospital/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    internal class Manager
+    {
+        public void Iniciar()
+        {
+
+        }
+    }
+}
