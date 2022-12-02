a16477ca8b785e0250d95a96c71ae4b3780408e9
airplanedana <dana.gomez@airplane.solutions>
2022-12-02 12:20:08 +0100

    Clase Hospital y menu inicial

---
+++ Hospital/Hospital.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hospital
+{
+    internal class Hospital
+    {
+        private string nombre;
+        private string ubicacion;
+        private int camasDisponibles;
+        private int camasOcupadas;
+        private List<Paciente> pacientes;
+
+        public Hospital(string nombre, string ubicacion)
+        {
+            this.nombre = nombre;
+            this.ubicacion = ubicacion;
+            this.camasDisponibles = 20;
+            this.camasOcupadas = 0;
+            this.pacientes = new List<Paciente>();
+        }
+
+        public string GetNombre() { return this.nombre; }
+        public void SetNombre(string nombre) { this.nombre = nombre; }
+        public string GetUbicacion() { return this.ubicacion; }
+        public void SetUbicacion(string ubicacion) { this.ubicacion = ubicacion;}
+        public int GetCamasDisponibles() { return this.camasDisponibles; }
+        public void SetCamasDisponibles(int camasDisponibles) { this.camasDisponibles = camasDisponibles; }
+        public int GetCamasOcupadas() { return this.camasOcupadas; }
+        public void SetCamasOcupadas(int camasOcupadas) { this.camasOcupadas = camasOcupadas; }
+        public List<Paciente> GetPacientes() { return this.pacientes; }
+        public void SetPacientes(List<Paciente> pacientes) { this.pacientes = pacientes; }
+
+
+
+    }
+}
--- Hospital/Manager.cs
+++ Hospital/Manager.cs
@@ -10,7 +10,42 @@ namespace Hospital
     {
         public void Iniciar()
         {
+            // Mostrar menu con opciones
+            int opc = 0;
 
+            while (opc != 1 && opc != 2 && opc != 3 && opc != 4 && opc != 5)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Bienvenido. Que le gustaria hacer? ");
+                Console.WriteLine("1. Ingresar a un nuevo paciente.");
+                Console.WriteLine("2. Dar el alta a un paciente existente.");
+                Console.WriteLine("3. Realizar una prueba a un paciente.");
+                Console.WriteLine("4. Asignar medicación a un paciente.");
+                Console.WriteLine("5. Dar el alta a un paciente por muerte o curado.");
+
+                opc = Convert.ToInt32(Console.ReadLine());
+            }
+
+            switch (opc)
+            {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
