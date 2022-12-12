ee7843eff737f259c93ac0376dd4625e5f514f9e
airplanedana <dana.gomez@airplane.solutions>
2022-12-12 10:22:20 +0100

    Clase medicamento

---
+++ Hospital/Medicamento.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hospital
+{
+    internal class Medicamento
+    {
+        private string nombre;
+        private string indicaciones;
+        private string efectosSecundarios;
+        private string dosis;
+        private string efectosAdversos;
+        private double precio;
+
+        public Medicamento(string nombre)
+        {
+            this.nombre = nombre;
+            this.indicaciones = "";
+            this.efectosSecundarios = "";
+            this.dosis = "";
+            this.efectosAdversos = "";
+            this.precio = 0;
+        }
+
+        public Medicamento(string nombre, string indicaciones, string efectosSecundarios, string dosis, string efectosAdversos, double precio) : this(nombre)
+        {
+            this.indicaciones = indicaciones;
+            this.efectosSecundarios = efectosSecundarios;
+            this.dosis = dosis;
+            this.efectosAdversos = efectosAdversos;
+            this.precio = precio;
+        }
+
+        public string GetNombre() { return this.nombre; }
+        public string GetIndicaciones() { return this.indicaciones; }
+        public string GetEfectosSecundarios() { return this.efectosSecundarios; }
+        public string GetDosis() { return this.dosis; }
+        public string GetEfectosAdversos() { return this.efectosAdversos; }
+        public double GetPrecio() { return this.precio; }
+        public void SetNombre(string nombre) { this.nombre = nombre; }
+        public void SetIndicaciones(string indicaciones) { this.indicaciones = indicaciones; }
+        public void SetEfectosSecundarios(string efectosSecundarios) { this.efectosSecundarios = efectosSecundarios; }
+        public void SetDosis(string dosis) { this.dosis = dosis; }
+        public void SetEfectosAdversos(string efectosAdversos) { this.efectosAdversos = efectosAdversos; }
+        public void SetPrecio(double precio) { this.precio = precio; }
+
+
+    }
+}
--- Hospital/Paciente.cs
+++ Hospital/Paciente.cs
@@ -9,7 +9,7 @@ namespace Hospital
         private int diasIngresado;
         private List<string> diagnosticos;
         private List<char> pronosticos;
-        private List<string> medicaciones;
+        private List<Medicamento> medicaciones;
         private List<string> pruebas;
 
         public Paciente(string dni, string nombre, int edad, string direccion, int diasIngresado) : base(dni, nombre, edad, direccion)
@@ -17,19 +17,19 @@ namespace Hospital
             this.diasIngresado = diasIngresado;
             this.diagnosticos = new List<string>();
             this.pronosticos = new List<char>();
-            this.medicaciones = new List<string>();
+            this.medicaciones = new List<Medicamento>();
             this.pruebas = new List<string>();
         }
 
         public int GetDiasIngresado() { return this.diasIngresado; }
         public List<string> GetDiagnosticos() { return this.diagnosticos; }
         public List<char> GetPronosticos() { return this.pronosticos; }
-        public List<string> GetMedicaciones() { return this.medicaciones; }
+        public List<Medicamento> GetMedicaciones() { return this.medicaciones; }
         public List<string> GetPruebas() { return this.pruebas; }
         public void SetDiasIngresado(int diasIngresado) { this.diasIngresado = diasIngresado; }
         public void SetDiagnosticos(List<string> diagnosticos) { this.diagnosticos = diagnosticos; }
         public void SetPronosticos(List<char> pronosticos) { this.pronosticos = pronosticos; }
-        public void SetMedicaciones(List<string> medicaciones) { this.medicaciones = medicaciones; }
+        public void SetMedicaciones(List<Medicamento> medicaciones) { this.medicaciones = medicaciones; }
         public void SetPruebas(List<string> pruebas) { this.pruebas = pruebas; }
 
         public void RecibirDiagnostico(string diagnostico)
@@ -50,7 +50,7 @@ namespace Hospital
             this.pruebas.Add(prueba);
         }
 
-        public void RecibirMedicacion(string medicacion)
+        public void RecibirMedicacion(Medicamento medicacion)
         {
             if (!this.medicaciones.Contains(medicacion))
                 this.medicaciones.Add(medicacion);
